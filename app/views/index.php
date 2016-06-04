7835e1a7d3648fc05ba259a245ff7e05c3c293d3
Jeferson De Freitas <jdefreitaspinto@gmail.com>
2016-06-04 14:09:05 -0400

    mas cambios de rutas

--- app/views/index.php
+++ app/views/index.php
@@ -5,17 +5,17 @@
 </head>
 <body id="page-top" data-spy="scroll" data-target="navbar-fixed-top">
 	<?php echo $hello; ?>
-		<script src="js/jquery.js"></script>
+	<script src="/public/jquery/jquery.min.js"></script>
 
 	<!-- Bootstrap Core JavaScript -->
-	<script src="js/bootstrap.min.js"></script>
+	<script src="/public/grayscale/js/bootstrap.min.js"></script>
 
 	<!-- Plugin JavaScript -->
-	<script src="js/jquery.easing.min.js"></script>
+	<script src="/public/jquery/jquery.easing.min.js"></script>
 
 
 	<!-- Custom Theme JavaScript -->
-	<script src="js/grayscale.js"></script>
+	<script src="/public/grayscale/js/grayscale.js"></script>
 
 </body>
 </html>
