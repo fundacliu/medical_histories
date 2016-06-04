b55fcac72d3fc5e00030333d241853b36e16ee2b
Jeferson De Freitas <jdefreitaspinto@gmail.com>
2016-06-04 13:10:52 -0400

    haciendo arreglos

--- app/views/index.php
+++ app/views/index.php
@@ -3,7 +3,19 @@
 <head>
 	<?php echo $head; ?>
 </head>
-<body>
+<body id="page-top" data-spy="scroll" data-target="navbar-fixed-top">
 	<?php echo $hello; ?>
+		<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Plugin JavaScript -->
+	<script src="js/jquery.easing.min.js"></script>
+
+
+	<!-- Custom Theme JavaScript -->
+	<script src="js/grayscale.js"></script>
+
 </body>
 </html>
