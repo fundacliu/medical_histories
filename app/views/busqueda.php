b62030cd0b47dd948366acc957698e0cc23e0767
Jessi De Pin <lilijess.yo@gmail.com>
2016-06-04 20:14:58 -0400

    Cambios visuales en general

--- app/views/busqueda.php
+++ app/views/busqueda.php
@@ -1,10 +1,11 @@
 <!-- Busqueda -->
-<section id="download" class="content-section text-center">
+<section class="content-section text-center">
     <div class="download-section">
-        <div class="container">
+        <div class="container" id="download">
             <div class="col-lg-8 col-lg-offset-2">
                 <h2 class="TituloGeneral Handlee Completo">Busqueda</h2>
-                <a href="#" class="btn btn-default btn-lg">Visit Download Page</a>
+				<input class="Barra" type="text" name="Cedula" placeholder="Ingrese Identificador">
+                <a href="#" class="btn btn-default btn-lg">Buscar</a>
             </div>
         </div>
     </div>
