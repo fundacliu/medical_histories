a886584658fcab1aa304fe221c3e69f2815928f8
jefer <jdefreitaspinto@gmail.com>
2016-06-19 22:23:34 -0400

    fix choco orm

--- lib/choco/orm.php
+++ lib/choco/orm.php
@@ -1,6 +1,6 @@
 <?php
 namespace Choco;
-//require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/src/yaml.php';
 require_once __DIR__ . '/src/factory.php';
 
